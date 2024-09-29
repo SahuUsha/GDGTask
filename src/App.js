174fbe44ce89125e92d7357eee77a576acb97227
SahuUsha <sahuur@rknec.edu>
2024-09-29 22:54:27 +0530

    Update App.js with changes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
           <Route path='/Contact' element={<Contact />} />
           <Route path='/Alumni' element={<Alumni />} />
           <Route path='/Event' element={<Eventcom />} />
